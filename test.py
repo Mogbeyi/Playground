2a39ed0cdfdb27afbbda1cc7b79165c725c0ac12
Emmy Wonder <emmywonder@Emmys-MacBook-Pro.local>
2020-11-03 00:02:27 +0100

    add test file

---
+++ test.py
@@ -0,0 +1,20 @@
+def sum_list(arr):
+    if len(arr) == 1:
+        return arr[0] 
+    return arr[0] + sum_list(arr[1:])
+
+def add_list(arr):
+    if len(arr) == 0:
+        return 0
+    return arr[0][0] + arr[0][1] + add_list(arr[1:])
+
+def count_from_one(x):
+     
+    def count(start):
+        if start > x:
+            return []
+        return [start] + count(start + 1)
+
+    return count(1)
+
+
