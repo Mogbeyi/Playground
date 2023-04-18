99b8fc58d87073e0ad69cde30e534e8fafebb9f9
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2023-04-18 12:25:31 +0100

    Use decorators

--- decorator.py
+++ decorator.py
@@ -1,11 +1,22 @@
-def double(fn):
-    def result(a):
-        return 2 * fn(a)
-    return result
+#def double(fn):
+#    def result(a):
+#        return 2 * fn(a)
+#    return result
 
-@double
+#@double
+#def square(x):
+#    return x * x
+
+##print(double(square)(4)) is equivalent to print(square(4))
+#print(square(4))
+
+def add_two(fn):
+    value = fn(a)
+    
+    return value + 2
+
+@add_two()
 def square(x):
     return x * x
 
-#print(double(square)(4)) is equivalent to print(square(4))
 print(square(4))
